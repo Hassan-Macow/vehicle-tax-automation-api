96673a2b673d8e21c23caa1fdc2c8923faee05b4
Hassan Maow Mohamud <114898695+Hassan-Macow@users.noreply.github.com>
2025-04-19 09:27:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,24 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
-import cv2
-import numpy as np
-from ultralytics import YOLO
+import cv2, numpy as np, os
 import easyocr
-import os
-import requests
+from ultralytics import YOLO
 import gdown
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["*"], allow_credentials=True,
+    allow_methods=["*"], allow_headers=["*"]
 )
 
-
-
-# Download model from Google Drive
 def download_model():
     if not os.path.exists("best.pt"):
-        print("Downloading model from Google Drive with gdown...")
-        url = "https://drive.google.com/uc?id=1YiHtOJMuN3zt1JnyOmADwemU_Sm4751D"
-        gdown.download(url, "best.pt", quiet=False)
+        print("Downloading model...")
+        gdown.download("https://drive.google.com/uc?id=1YiHtOJMuN3zt1JnyOmADwemU_Sm4751D", "best.pt", quiet=False)
 
 download_model()
-
-
-model = YOLO("best.pt")  # Trained YOLOv8 model
 reader = easyocr.Reader(['en'], gpu=False)
 
 @app.post("/detect-plate")
@@ -39,14 +27,15 @@ async def detect_plate(file: UploadFile = File(...)):
     nparr = np.frombuffer(contents, np.uint8)
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
+    model = YOLO("best.pt")
     results = model(image)
 
     for result in results:
         for box in result.boxes:
             x1, y1, x2, y2 = map(int, box.xyxy[0])
             plate_img = image[y1:y2, x1:x2]
-            text_result = reader.readtext(plate_img)
-            text = text_result[0][1] if text_result else "Not Detected"
+            result = reader.readtext(plate_img)
+            text = result[0][1] if result else "Not Detected"
             return {"plate_number": text}
 
     return {"plate_number": "No plate found"}
